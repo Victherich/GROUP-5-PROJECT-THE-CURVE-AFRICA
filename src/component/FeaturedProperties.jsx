5747415e720386c63227c7b05b55ee9328560f5b
Victherich <victherich@gmail.com>
2024-02-23 11:54:24 +0100

    H

--- src/component/FeaturedProperties.jsx
+++ src/component/FeaturedProperties.jsx
@@ -1,115 +1,115 @@
 
-// import React from 'react'
-// import "../CSS/FeaturedProperties.css"
-// import img1 from "../Images/first.jpg"
-// import img2 from "../Images/second.jpg"
-// import img3 from "../Images/third.jpg"
-// import img4 from "../Images/fourth.jpeg"
-// import img5 from "../Images/fifth.jpg"
+import React from 'react'
+import "../CSS/FeaturedProperties.css"
+import img1 from "../Images/first.jpg"
+import img2 from "../Images/second.jpg"
+import img3 from "../Images/third.jpg"
+import img4 from "../Images/fourth.jpeg"
+import img5 from "../Images/fifth.jpg"
 
-// const FeaturedProperties = () => {
-//   return (
-//     <div className='featureddiv'>
-//         <div className='featured1'>
-//             <h1>Featured Properties</h1>
-//         </div>
+const FeaturedProperties = () => {
+  return (
+    <div className='featureddiv'>
+        <div className='featured1'>
+            <h1>Featured Properties</h1>
+        </div>
 
-//         <div className='featured2'>
-//             <div className='featured3'>
-//                 <div className='featuredimg'>
-//                     <img src={img1} alt="" />
-//                 </div>
+        <div className='featured2'>
+            <div className='featured3'>
+                <div className='featuredimg'>
+                    <img src={img1} alt="" />
+                </div>
 
-//                 <div className='featuredtext'>
-//                     <h3>Park view Estate, Lagos</h3>
-//                     <div className='featuredtextspan'>
-//                         <span>Category: For rent</span>
-//                         <span>Price: #900,000</span>
-//                         <span>Location: Lagos</span>
-//                     </div>
-//                     <div className='featuredbtndiv'>
-//                         <button className='view'>View</button>
-//                         <button className='add'>Add to favourite</button>
-//                     </div>
-//                 </div>
-//             </div>
+                <div className='featuredtext'>
+                    <h3>Park view Estate, Lagos</h3>
+                    <div className='featuredtextspan'>
+                        <span>Category: For rent</span>
+                        <span>Price: #900,000</span>
+                        <span>Location: Lagos</span>
+                    </div>
+                    <div className='featuredbtndiv'>
+                        <button className='view'>View</button>
+                        <button className='add'>Add to favourite</button>
+                    </div>
+                </div>
+            </div>
 
-//             <div className='featured3'>
-//                 <div className='featuredimg'>
-//                     <img src={img2} alt="" />
-//                 </div>
+            <div className='featured3'>
+                <div className='featuredimg'>
+                    <img src={img2} alt="" />
+                </div>
 
-//                 <div className='featuredtext'>
-//                     <h3>Park view Estate, Lagos</h3>
-//                     <div className='featuredtextspan'>
-//                         <span>Category: For rent</span>
-//                         <span>Price: #900,000</span>
-//                         <span>Location: Lagos</span>
-//                     </div>
-//                     <div className='featuredbtndiv'>
-//                         <button className='view'>View</button>
-//                         <button className='add'>Add to favourite</button>
-//                     </div>
-//                 </div>
-//             </div>
+                <div className='featuredtext'>
+                    <h3>Park view Estate, Lagos</h3>
+                    <div className='featuredtextspan'>
+                        <span>Category: For rent</span>
+                        <span>Price: #900,000</span>
+                        <span>Location: Lagos</span>
+                    </div>
+                    <div className='featuredbtndiv'>
+                        <button className='view'>View</button>
+                        <button className='add'>Add to favourite</button>
+                    </div>
+                </div>
+            </div>
 
-//             <div className='featured3'>
-//                 <div className='featuredimg'>
-//                     <img src={img3} alt="" />
-//                 </div>
+            <div className='featured3'>
+                <div className='featuredimg'>
+                    <img src={img3} alt="" />
+                </div>
 
-//                 <div className='featuredtext'>
-//                     <h3>Park view Estate, Lagos</h3>
-//                     <div className='featuredtextspan'>
-//                         <span>Category: For rent</span>
-//                         <span>Price: #900,000</span>
-//                         <span>Location: Lagos</span>
-//                     </div>
-//                     <div className='featuredbtndiv'>
-//                         <button className='view'>View</button>
-//                         <button className='add'>Add to favourite</button>
-//                     </div>
-//                 </div>
-//             </div>
+                <div className='featuredtext'>
+                    <h3>Park view Estate, Lagos</h3>
+                    <div className='featuredtextspan'>
+                        <span>Category: For rent</span>
+                        <span>Price: #900,000</span>
+                        <span>Location: Lagos</span>
+                    </div>
+                    <div className='featuredbtndiv'>
+                        <button className='view'>View</button>
+                        <button className='add'>Add to favourite</button>
+                    </div>
+                </div>
+            </div>
 
-//             <div className='featured3'>
-//                 <div className='featuredimg'>
-//                     <img src={img4} alt="" />
-//                 </div>
+            <div className='featured3'>
+                <div className='featuredimg'>
+                    <img src={img4} alt="" />
+                </div>
 
-//                 <div className='featuredtext'>
-//                     <h3>Park view Estate, Lagos</h3>
-//                     <div className='featuredtextspan'>
-//                         <span>Category: For rent</span>
-//                         <span>Price: #900,000</span>
-//                         <span>Location: Lagos</span>
-//                     </div>
-//                     <div className='featuredbtndiv'>
-//                         <button className='view'>View</button>
-//                         <button className='add'>Add to favourite</button>
-//                     </div>
-//                 </div>
-//             </div>
+                <div className='featuredtext'>
+                    <h3>Park view Estate, Lagos</h3>
+                    <div className='featuredtextspan'>
+                        <span>Category: For rent</span>
+                        <span>Price: #900,000</span>
+                        <span>Location: Lagos</span>
+                    </div>
+                    <div className='featuredbtndiv'>
+                        <button className='view'>View</button>
+                        <button className='add'>Add to favourite</button>
+                    </div>
+                </div>
+            </div>
 
-//             <div className='featured3'>
-//                 <div className='featuredimg'>
-//                     <img src={img5} alt="" />
-//                 </div>
+            <div className='featured3'>
+                <div className='featuredimg'>
+                    <img src={img5} alt="" />
+                </div>
 
-//                 <div className='featuredtext'>
-//                     <h3>Park view Estate, Lagos</h3>
-//                     <div className='featuredtextspan'>
-//                         <span>Category: For rent</span>
-//                         <span>Price: #900,000</span>
-//                         <span>Location: Lagos</span>
-//                     </div>
-//                     <div className='featuredbtndiv'>
-//                         <button className='view'>View</button>
-//                         <button className='add'>Add to favourite</button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-// </div>
-// )}
-// export default FeaturedProperties
+                <div className='featuredtext'>
+                    <h3>Park view Estate, Lagos</h3>
+                    <div className='featuredtextspan'>
+                        <span>Category: For rent</span>
+                        <span>Price: #900,000</span>
+                        <span>Location: Lagos</span>
+                    </div>
+                    <div className='featuredbtndiv'>
+                        <button className='view'>View</button>
+                        <button className='add'>Add to favourite</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+)}
+export default FeaturedProperties
